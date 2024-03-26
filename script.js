fab6f863395c4453cd2919c729ced354a6e8b428
brkhzr <162829783+brkhzr@users.noreply.github.com>
2024-03-26 22:52:49 +0300

    siz tabi ancauyuyunbelediyesizinsearchbarıdüzeltir

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function arama() {
   let input = document.getElementById('searchbar').value
   input = input.toLowerCase();
-  let x = document.getElementsByClassName('custom-btn btn-8');
+  let x = document.getElementsByClassName('btn-8');
  
   for (i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
